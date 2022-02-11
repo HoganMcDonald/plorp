6e52aefea945eece67718929a3e6cdc62fddcef9
Hogan McDonald <hogan.developer@gmail.com>
2022-02-11 11:06:45 -0600

    creates api helper for making json requests

---
+++ app/javascript/helpers/api.js
@@ -0,0 +1,19 @@
+const csrfToken = document.querySelector('meta[name="csrf-token"]').content
+
+export const post = async (url, params) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': csrfToken,
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(params),
+  })
+
+  const body = await response.json()
+}
