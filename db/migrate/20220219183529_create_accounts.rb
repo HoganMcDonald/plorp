df19a8796257802684483f50aca82c9b36f3454f
Hogan McDonald <hogan.developer@gmail.com>
2022-02-19 12:37:01 -0600

    creates accounts

---
+++ db/migrate/20220219183529_create_accounts.rb
@@ -0,0 +1,15 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :plaid_id
+      t.integer :account_type
+      t.integer :account_sub_type
+      t.decimal :balance
+      t.decimal :limit
+      t.references :authorization, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_041540) do
+ActiveRecord::Schema.define(version: 2022_02_19_183529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.string "plaid_id"
+    t.integer "account_type"
+    t.integer "account_sub_type"
+    t.decimal "balance"
+    t.decimal "limit"
+    t.bigint "authorization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["authorization_id"], name: "index_accounts_on_authorization_id"
+  end
 
   create_table "authorizations", force: :cascade do |t|
     t.string "access_token"
@@ -35,4 +48,5 @@ ActiveRecord::Schema.define(version: 2022_02_19_041540) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "authorizations"
 end
